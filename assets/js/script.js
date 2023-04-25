9bcffc1df23a49f9e4446a73f0fd099c3c7db25e
irene <panisirene@gmail.com>
2023-04-24 22:10:38 -0700

    Declare global variables needed

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+// object content
+  // question: (array of questions)
+  // options: (array of arrays of options)
+  // answers: (array of correct answers)
+var content = {
+  questions: [
+    'What is the purpose of the "this" keyword in JavaScript?',
+    'What is the correct way to declare a variable in JavaScript?',
+    'Which JavaScript method is used to add new elements to an array?',
+    'What is the result of the following code: 1 + "2" + 3?',
+    'What is the difference between "==" and "===" in JavaScript?'
+  ],
+  options: [
+    ['To refer to the current object', 'To define a new variable', 'To create a loop', 'To compare two values'],
+    ['x = 3', 'v x = 3', 'variable x = 3', 'var x = 3'],
+    ['slice()', 'push()', 'pop()', 'shift()'],
+    ['"123"', '6', '123', 'Syntax error'],
+    ['"==" compares both the values and the data types of two variables, while "===" compares only the values.', '"===" compares both the values and the data types of two variables, while "==" compares only the values.', 'There is no difference, "==" and "===" are interchangeable.', '"===" is not a valid operator in JavaScript.']
+  ],
+  answers: [
+    'To refer to the current object',
+    'var x = 3',
+    'push()',
+    '"123"',
+    '"===" compares both the values and the data types of two variables, while "==" compares only the values.'
+  ]
+};
+
+// array of player highscores
+    // players should be objects w/ name and score properties
+var players = [];
+
+// function startGame
+    
+// function startTimer
+
+// function displayQuestion
+    // changes html and displays content.question and content.options
+    // addEventListener to buttons
+    // if isCorrectAnswer append "Correct!" then return
+    // if !isCorrectAnswer -15 timer append "Wrong!" then return
+
+// function isCorrectAnswer 
+    // if clicked === right answer return true
+    // default return false
+
+// function endGame
+    // displays end screen
+
+// function savePlayer
+    // create object then save object to array
+    // use local storage to update array
+
+// function displayScores
